193341f1de2ce5e43551485e0033c1e321f644a7
CamINKu <caminkunick@gmail.com>
2026-06-22 18:35:55 +0700

    feat: add getStatus endpoint to SettingController

--- src/setting/setting.controller.ts
+++ src/setting/setting.controller.ts
@@ -5,6 +5,13 @@ import { SettingService } from './setting.service';
 export class SettingController {
   constructor(private readonly settingService: SettingService) {}
 
+  @Get()
+  async getStatus() {
+    return {
+      status: 'ok',
+    };
+  }
+
   @Get('id/:id')
   async getSettingById(@Param('id') id: string) {
     const result = await this.settingService
